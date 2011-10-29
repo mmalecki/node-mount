696b8131fcda2b7c03001872f541f04087351b31
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-10-30 01:27:51 +0200

    [mount] Add basic mount function

---
+++ src/mount.cc
@@ -0,0 +1,25 @@
+#include <sys/mount.h>
+#include <v8.h>
+#include <node.h>
+
+v8::Handle<v8::Value> Mount(const v8::Arguments &args) {
+  v8::HandleScope scope;
+
+  if (args.Length() < 3) {
+    // TODO: make it raise a proper exception
+    return v8::ThrowException(v8::String::New("`mount` needs at least 3 parameters"));
+  }
+  
+  v8::String::Utf8Value device(args[0]->ToString());
+  v8::String::Utf8Value path(args[1]->ToString());
+  v8::String::Utf8Value type(args[2]->ToString());
+
+  // TODO: expose flag values
+  return (mount(*device, *path, *type, 0, NULL) == 0) ? v8::True(): v8::False();
+}
+
+void init (v8::Handle<v8::Object> target) {
+  NODE_SET_METHOD(target, "mount", Mount);
+}
+NODE_MODULE(mount, init)
+
