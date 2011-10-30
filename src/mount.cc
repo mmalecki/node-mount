955f362edbacd02d01e145c0a1c0b9e03a211706
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-10-30 02:03:30 +0200

    [mount] Add `umount` function

--- src/mount.cc
+++ src/mount.cc
@@ -18,8 +18,21 @@ v8::Handle<v8::Value> Mount(const v8::Arguments &args) {
   return (mount(*device, *path, *type, 0, NULL) == 0) ? v8::True() : v8::False();
 }
 
+v8::Handle<v8::Value> Umount(const v8::Arguments &args) {
+  v8::HandleScope scope;
+
+  if (args.Length() < 1) {
+    return v8::ThrowException(v8::String::New("`umount` needs at least 1 parameter"));
+  }
+
+  v8::String::Utf8Value path(args[0]->ToString());
+
+  return (umount(*path) == 0) ? v8::True() : v8::False();
+}
+
 void init (v8::Handle<v8::Object> target) {
   NODE_SET_METHOD(target, "mount", Mount);
+  NODE_SET_METHOD(target, "umount", Umount);
 }
 NODE_MODULE(mount, init)
 
